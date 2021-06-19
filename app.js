570bd84afb4070567a9f6d753d4f9d847567c709
Ivan Ivanitskiy <iivanitskiy@mail.ru>
2021-06-19 13:56:31 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const board = document.querySelector('#board')
+const colors = ['deepskyblue', 'greenyellow', 'indigo', 'magenta', 'orangered', 'crimson', 'gold']
+const SQUARES_NUMBER = 840
+
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement('div')
+  square.classList.add('square')
+
+  square.addEventListener('mouseover', setColor)
+
+  square.addEventListener('mouseleave', removeColor)
+
+  board.append(square)
+}
+
+function setColor(e) {
+  const element = e.target
+  const color = getRandomColor()
+  element.style.backgroundColor = color
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(e) {
+  const element = e.target
+  element.style.backgroundColor = '#1D1D1D'
+  element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)]
+}
